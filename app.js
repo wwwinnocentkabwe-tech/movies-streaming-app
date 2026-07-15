7330710f5506557cb5c5865d5a480c94bed3e42a
wwwinnocentkabwe-tech <www.innocentkabwe@gmail.com>
2026-07-15 23:24:56 +0200

    fix missing closing bracket in 404 handler

--- app.js
+++ app.js
@@ -94,7 +94,6 @@ app.use((err, req, res, next) => {
 app.use((req, res) => {
   res.status(404).json({ success: false, message: 'Route not found' });
 });
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
