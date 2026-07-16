fa7f6f9df858b8dfe11756e8c4f5db68440674a5
wwwinnocentkabwe-tech <www.innocentkabwe@gmail.com>
2026-07-16 10:30:47 +0200

    Add OPTIONS to CORS allowed methods to fix preflight failures

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(helmet());
 const corsOptions = {
   origin: process.env.CORS_ORIGIN?.split(',') || ['http://localhost:3000', 'http://localhost:5173'],
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 };
 const storageDir = path.join(__dirname, 'middleware', 'Storage');
