534d2d9798d58af8ff7c4a50e54ee11a7edb83b4
wwwinnocentkabwe-tech <www.innocentkabwe@gmail.com>
2026-08-11 17:33:06 +0200

    Force IPv4 for Nodemailer SMTP connection

--- utils/email.js
+++ utils/email.js
@@ -7,6 +7,7 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,
   },
+  family: 4,
 });
 
 const sendResetEmail = async (toEmail, resetLink) => {
