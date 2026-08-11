d71495527743f120c568cfcd574406b01aef0356
wwwinnocentkabwe-tech <www.innocentkabwe@gmail.com>
2026-08-11 17:18:58 +0200

    Force IPv4 DNS resolution to fix Nodemailer ENETUNREACH on Render

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 // app.js
+const dns = require('dns');
+   dns.setDefaultResultOrder('ipv4first');
+   
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
